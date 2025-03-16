d8a999d0312920d61305036c52a8e6e0bdfad972
lipnelz <174376617+lipnelz@users.noreply.github.com>
2025-03-16 09:10:22 +0100

    feat: add get_addresses function which aims to get specific data about the address

--- script.py
+++ script.py
@@ -4,27 +4,29 @@ import re
 
 
 def extract_address_from_file(file_path):
-    # Expression régulière pour correspondre à l'adresse
-    address_pattern = r'AU[1-9A-HJ-NP-Za-km-z]{50}'
+    """
+    Reads a text file and returns its content as a string.
 
+    :param file_path: The name of the file to read.
+    :return: The content of the file as a string.
+    """
     try:
-        # Lire le contenu du fichier
+        # Open the file in read mode
         with open(file_path, 'r') as file:
-            content = file.read()
-
-        # Rechercher l'adresse dans le contenu du fichier
-        match = re.search(address_pattern, content)
-
-        if match:
-            return match.group(0)
-        else:
-            return "Aucune adresse trouvée."
-
+            content = file.read().strip()  # Read and remove leading and trailing spaces
+        return content
     except FileNotFoundError:
-        return "Le fichier spécifié n'existe pas."
+        return "The specified file does not exist."
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 
 def get_status(address: str) -> None:
+    """
+    Print the status from a given address
+
+    :param address: The address to querry.
+    """
     # Define API and URL
     url = 'https://mainnet.massa.net/api/v2'
     headers = {
@@ -54,7 +56,39 @@ def get_status(address: str) -> None:
     except requests.exceptions.RequestException as e:
         print(f"Une erreur s'est produite : {e}")
 
+
+def get_addresses(address: str) -> None:
+    # Define API and URL
+    url = 'https://mainnet.massa.net/api/v2'
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    # Define requests data
+    data = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "get_addresses",
+         "params": [[address]]
+    }
+
+    try:
+        # Send POST request
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        # Check response status
+        if response.status_code == 200:
+            # Parse JSON
+            response_json = response.json()
+            # Correctly display JSON
+            print(json.dumps(response_json, indent=4))
+        else:
+            print(f"Erreur: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Une erreur s'est produite : {e}")
+
 if __name__ == "__main__":
     address = extract_address_from_file('address.txt')
     print("Querry on: " + address)
-    get_status(address)
+    #get_status(address)
+    get_addresses(address)
