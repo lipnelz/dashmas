af4c0cc0085317a13c53dd307cdfe69929db001d
lipnelz <174376617+lipnelz@users.noreply.github.com>
2025-03-16 14:37:21 +0100

    chore: rename script into main and update graphical interface with refresh button

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from matplotlib.ticker import ScalarFormatter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import re
 
-def extract_address_from_file(file_path):
+def extract_address_from_file(file_path: str) -> str:
     """
     Reads a text file and returns its content as a string.
 
@@ -35,8 +35,6 @@ def get_status(address: str) -> None:
     headers = {
         'Content-Type': 'application/json'
     }
-
-    # Define requests data
     data = {
         "jsonrpc": "2.0",
         "id": 1,
@@ -47,27 +45,22 @@ def get_status(address: str) -> None:
     try:
         # Send POST request
         response = requests.post(url, headers=headers, data=json.dumps(data))
-
         # Check response status
         if response.status_code == 200:
             # Parse JSON
             response_json = response.json()
-            # Correctly display JSON
             print(json.dumps(response_json, indent=4))
         else:
-            print(f"Erreur: {response.status_code}")
+            print(f"Error: {response.status_code}")
     except requests.exceptions.RequestException as e:
-        print(f"Une erreur s'est produite : {e}")
+        print(f"An error occurred: {e}")
 
-
-def get_addresses(address: str) -> str:
+def get_addresses(address: str) -> dict:
     # Define API and URL
     url = 'https://mainnet.massa.net/api/v2'
     headers = {
         'Content-Type': 'application/json'
     }
-
-    # Define requests data
     data = {
         "jsonrpc": "2.0",
         "id": 1,
@@ -78,30 +71,76 @@ def get_addresses(address: str) -> str:
     try:
         # Send POST request
         response = requests.post(url, headers=headers, data=json.dumps(data))
-
         # Check response status
         if response.status_code == 200:
             # Parse JSON
             response_json = response.json()
-            # Correctly display JSON
             print(json.dumps(response_json, indent=4))
             return response_json
         else:
-            print(f"Erreur: {response.status_code}")
+            print(f"Error: {response.status_code}")
+            return {}
     except requests.exceptions.RequestException as e:
-        print(f"Une erreur s'est produite : {e}")
+        print(f"An error occurred: {e}")
+        return {}
+
+def extract_data(json_data: dict) -> Tuple[str, int, List[int], List[int]]:
+    """
+    Extract useful JSON data.
+
+    :param json_data: Input JSON data to parse.
+    :return: Tuple composed of final_balance, final_roll_count, cycles, and ok_counts.
+    """
+    if "result" in json_data and len(json_data["result"]) > 0:
+        result = json_data["result"][0]
+        final_balance = result["final_balance"]
+        final_roll_count = result["final_roll_count"]
+        cycles = [info["cycle"] for info in result["cycle_infos"]]
+        ok_counts = [info["ok_count"] for info in result["cycle_infos"]]
+        return final_balance, final_roll_count, cycles, ok_counts
+    return "", 0, [], []
+
+def refresh_data(address: str, label_balance: tk.Label, label_roll_count: tk.Label, ax: plt.Axes, canvas: FigureCanvasTkAgg) -> None:
+    """
+    Refresh data and update the UI.
+
+    :param address: Massa node address.
+    :param label_balance: Label balance object.
+    :param label_roll_count: Label roll count object.
+    :param ax: Axes object.
+    :param canvas: Canvas object.
+    """
+    # Get new data
+    json_data = get_addresses(address)
+
+     # Extract useful data using the function
+    final_balance, final_roll_count, cycles, ok_counts = extract_data(json_data)
+
+    # Update labels
+    label_balance.config(text=f"Final Balance: {final_balance}")
+    label_roll_count.config(text=f"Final Roll Count: {final_roll_count}")
+
+    # Update plot
+    ax.clear()
+    ax.plot(cycles, ok_counts, marker='o', linestyle='-', color='b')
+    ax.set_title('OK Count par Cycle')
+    ax.set_xlabel('Cycle')
+    ax.set_ylabel('OK Count')
+    ax.grid(True)
+    ax.xaxis.set_major_formatter(ScalarFormatter(useOffset=False))
+    canvas.draw()
+
+###############################################################################
 
 if __name__ == "__main__":
     address = extract_address_from_file('address.txt')
     print("Querry on: " + address)
+
     #get_status(address)
     json_data = get_addresses(address)
 
-    # Extract usefull data
-    final_balance = json_data["result"][0]["final_balance"]
-    final_roll_count = json_data["result"][0]["final_roll_count"]
-    cycles = [info["cycle"] for info in json_data["result"][0]["cycle_infos"]]
-    ok_counts = [info["ok_count"] for info in json_data["result"][0]["cycle_infos"]]
+    # Extract useful data using the function
+    final_balance, final_roll_count, cycles, ok_counts = extract_data(json_data)
 
     # Create main window
     root = tk.Tk()
@@ -131,6 +170,10 @@ if __name__ == "__main__":
     canvas = FigureCanvasTkAgg(fig, master=plot_frame)
     canvas.draw()
     canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+    # Create Refresh Button
+    refresh_button = tk.Button(root, text="Refresh", command=lambda: refresh_data(address, label_balance, label_roll_count, ax, canvas))
+    refresh_button.pack(pady=5)
 
     # Run app
     root.mainloop()
