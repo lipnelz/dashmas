5f08919a50ba7ffb91d18656130d222515339100
lipnelz <174376617+lipnelz@users.noreply.github.com>
2025-03-22 16:18:19 +0100

    chore: update reponse code check

--- jrequests.py
+++ jrequests.py
@@ -23,7 +23,7 @@ def get_status(address: str) -> None:
         # Send POST request
         response = requests.post(url, headers=headers, data=json.dumps(data))
         # Check response status
-        if response.status_code == 200:
+        if response.status_code == requests.codes.ok::
             # Parse JSON
             response_json = response.json()
             print(json.dumps(response_json, indent=4))
@@ -49,7 +49,7 @@ def get_addresses(address: str) -> dict:
         # Send POST request
         response = requests.post(url, headers=headers, data=json.dumps(data))
         # Check response status
-        if response.status_code == 200:
+        if response.status_code == requests.codes.ok::
             # Parse JSON
             response_json = response.json()
             print(json.dumps(response_json, indent=4))
